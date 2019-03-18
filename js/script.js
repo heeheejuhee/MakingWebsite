28dc509fc7afdcdae71acbfdc831ad05d7f7f7fc
Juhee Han <200859841@cit3-501m-08.acg.ac.nz>
2019-03-18 16:10:19 +1300

    Section2added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//   $('.home-banner-slick').slick({
+//   	arrows: true,
+//   	autoplay: false,
+//   	responsive:[
+//   	{
+//   		breakpoint: 960,
+//   		settings:{
+//   			arrows: false,
+//   			autoplay: true,
+//   			autoplaySpeed: 4000,
+//   		}
+//   	}
+//   	]
+//   });
+
+// });
+// $('.bars').on('click',function(){
+//       $('.nav').slideToggle();
+//   });
+
+
+$(function(){
+
   $('.home-banner-slick').slick({
-  	arrows: true,
-  	autoplay: false,
-  	responsive:[
-  	{
-  		breakpoint: 960,
-  		settings:{
-  			arrows: false,
-  			autoplay: true,
-  			autoplaySpeed: 2000,
-  		}
-  	}
-  	]
+   arrows: true,
+   autoplay: false,
+   responsive:[
+   {
+     breakpoint: 960,
+     settings:{
+       arrows: false,
+       autoplay: true,
+       autoplaySpeed: 4000,
+     }
+   }
+   ]
   });
+
+
+  $('.bars').on('click',function(){
+      $('.nav').slideToggle();
+  });
+
 });
